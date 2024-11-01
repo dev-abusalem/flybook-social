c8d9c19fc15bcf10b023b111cf0d462677700c3f
Md Abu Salem <96655281+dev-abusalem@users.noreply.github.com>
2024-11-01 18:30:03 +0600

    update

--- app/(server)/api/post/create/route.ts
+++ app/(server)/api/post/create/route.ts
@@ -1,27 +1,32 @@
-import { prisma } from "@/app/lib/prisma"
-import { NextResponse } from "next/server"
-export async function POST(req: Response) {
-    try {
-      // Parse the request body
-      const { caption, imageUrl, userId } = await req.json()
-      
-      // Validate the input
-      if (!caption || !userId) {
-        return NextResponse.json( {error: 'Caption and userId are required.' } , {status:404})
-      }
-  
-      // Create a new post in the database
-      const newPost = await prisma.post.create({
-        data: {
-          caption,
-          imageUrl: imageUrl || null,
-          userId,
-        },
-      });
-      return NextResponse.json( {post: newPost } , {status:201})
-    } catch (error) {
-      console.error('Error creating post:', error);
-       return NextResponse.json( {error: 'Failed to create post' } , {status:500})
+import { prisma } from "@/app/lib/prisma";
+import { NextResponse } from "next/server";
+export async function POST(req: Request) {
+  try {
+    // Parse the request body
+    const { caption, imageUrl, userId } = await req.json();
 
+    // Validate the input
+    if (!caption || !userId) {
+      return NextResponse.json(
+        { error: "Caption and userId are required." },
+        { status: 404 }
+      );
     }
+
+    // Create a new post in the database
+    const newPost = await prisma.post.create({
+      data: {
+        caption,
+        imageUrl: imageUrl || null,
+        userId,
+      },
+    });
+    return NextResponse.json({ post: newPost }, { status: 201 });
+  } catch (error) {
+    console.error("Error creating post:", error);
+    return NextResponse.json(
+      { error: "Failed to create post" },
+      { status: 500 }
+    );
   }
+}
